89a5c7b89201d9371bf4d905dac27e257f3d2744
JamieWilson2000 <jamiewilson2000@yahoo.com>
2021-04-21 10:48:26 +0000

    Re-writing script3.js. Generating numbers, calculating answers in a simple version of the game

--- assets/javascript/script3.js
+++ assets/javascript/script3.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit")
+                checkAnswer();
             } else {
                 let startGame = this.getAttribute("data-type");
-                runGame(gameType);
+                runGame("easyLevel");
             }
         });
     }
@@ -17,4 +17,40 @@ document.addEventListener("DOMContentLoaded", function () {
 function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 10) + 1;
     let num2 = Math.floor(Math.random() * 10) + 1;
+
+    if (gameType === "easyLevel") {
+        displayEasyLevel(num1, num2)
+    }
+}
+
+function displayEasyLevel(easytopnum, easybotnum) {
+    document.getElementById("easytopnum").textContent = easytopnum;
+    document.getElementById("easybotnum").textContent = easybotnum;
+    document.getElementById("operator").textContent = "+";
+}
+
+function calculateCorrectAnswer() {
+    let easytopnum = parseInt(document.getElementById("easytopnum").innerText);
+    let easybotnum = parseInt(document.getElementById("easybotnum").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [easytopnum + easybotnum, "easyLevel"];
+    } else {
+        alert("fuck it");
+    }
+
+}
+
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        ;
+    } else {
+        alert("Wrong");
+    }
+    runGame(calculatedAnswer[1]);
 }
